642cdf31d836c391e56c6cc1c2e437102130e919
Muhammad Ismayil <85330544+Ismayil77@users.noreply.github.com>
2025-05-29 15:12:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,52 +1,4 @@
-
-// Add this at the top with other variables
-let countryTooltip;
-
-// Update the onEachCountryFeature function:
-function onEachCountryFeature(feature, layer) {
-    // Create a tooltip element
-    const tooltip = document.createElement('div');
-    tooltip.className = 'country-label';
-    tooltip.textContent = feature.properties.name || 'Unknown Country';
-    document.body.appendChild(tooltip);
-    tooltip.style.display = 'none';
-
-    layer.on({
-        mouseover: function(e) {
-            this.setStyle(highlightStyle);
-            this.bringToFront();
-            
-            // Show tooltip
-            if (feature.properties.name) {
-                tooltip.style.display = 'block';
-                countryTooltip = tooltip;
-            }
-        },
-        mouseout: function(e) {
-            countriesLayer.resetStyle(this);
-            
-            // Hide tooltip
-            if (countryTooltip) {
-                countryTooltip.style.display = 'none';
-            }
-        },
-        mousemove: function(e) {
-            // Update tooltip position
-            if (countryTooltip) {
-                const point = map.latLngToContainerPoint(e.latlng);
-                countryTooltip.style.left = `${point.x}px`;
-                countryTooltip.style.top = `${point.y}px`;
-            }
-        },
-        click: function(e) {
-            // Hide tooltip on click
-            if (countryTooltip) {
-                countryTooltip.style.display = 'none';
-            }
-            showCountryFoods(feature.properties, layer);
-        }
-    });
-}// Initialize the map with a neutral background
+// Initialize the map with a neutral background
 const map = L.map('map', {
     center: [20, 0],
     zoom: 2,
@@ -64,6 +16,7 @@ L.tileLayer('https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png', 
 let countriesGeoJson;
 let foodsData = {};
 let currentPopup = null;
+let countryTooltip;
 
 // Load both data files
 Promise.all([
@@ -105,25 +58,68 @@ function renderCountries() {
 
     // Fit map to show all countries
     map.fitBounds(countriesLayer.getBounds());
+}
 
-    function onEachCountryFeature(feature, layer) {
-        // Store country name in layer for reference
-        layer.feature = feature;
+function onEachCountryFeature(feature, layer) {
+    // Create a tooltip element
+    const tooltip = document.createElement('div');
+    tooltip.className = 'country-label';
+    tooltip.textContent = feature.properties.name || 'Unknown Country';
+    document.body.appendChild(tooltip);
+    tooltip.style.display = 'none';
 
-        // Mouseover events
-        layer.on({
-            mouseover: function(e) {
-                this.setStyle(highlightStyle);
-                this.bringToFront();
-            },
-            mouseout: function(e) {
-                countriesLayer.resetStyle(this);
-            },
-            click: function(e) {
-                showCountryFoods(feature.properties.name, layer);
+    // Store country name in layer for reference
+    layer.feature = feature;
+
+    // Mouseover events
+    layer.on({
+        mouseover: function(e) {
+            this.setStyle({
+                fillColor: '#e55039',
+                weight: 2,
+                color: '#fff',
+                fillOpacity: 0.8,
+                dashArray: '0'
+            });
+            this.bringToFront();
+            
+            // Show tooltip
+            if (feature.properties.name) {
+                tooltip.style.display = 'block';
+                countryTooltip = tooltip;
             }
-        });
-    }
+        },
+        mouseout: function(e) {
+            this.setStyle({
+                fillColor: '#f8c291',
+                weight: 1,
+                opacity: 1,
+                color: '#fff',
+                fillOpacity: 0.7,
+                dashArray: '0'
+            });
+            
+            // Hide tooltip
+            if (countryTooltip) {
+                countryTooltip.style.display = 'none';
+            }
+        },
+        mousemove: function(e) {
+            // Update tooltip position
+            if (countryTooltip) {
+                const point = map.latLngToContainerPoint(e.latlng);
+                countryTooltip.style.left = `${point.x}px`;
+                countryTooltip.style.top = `${point.y}px`;
+            }
+        },
+        click: function(e) {
+            // Hide tooltip on click
+            if (countryTooltip) {
+                countryTooltip.style.display = 'none';
+            }
+            showCountryFoods(feature.properties.name, layer);
+        }
+    });
 }
 
 function showCountryFoods(countryName, layer) {
@@ -173,8 +169,8 @@ function showCountryFoods(countryName, layer) {
         map.closePopup(currentPopup);
     });
 }
-// Add this at the bottom of your script.js
+
+// Clean up tooltips when map is destroyed
 map.on('unload', function() {
-    // Remove all tooltips when map is destroyed
     document.querySelectorAll('.country-label').forEach(el => el.remove());
 });
