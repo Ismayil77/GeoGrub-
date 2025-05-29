408383cb11d3d6d98ac189fecd2e8b831476a3c2
Muhammad Ismayil <85330544+Ismayil77@users.noreply.github.com>
2025-05-29 16:22:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -93,7 +93,7 @@ function showCountryFoods(countryName, layer) {
     // Create popup structure
     const popupContent = `
     <div class="food-popup">
-        <span class="close-popup">×</span>
+        //<span class="close-popup">×</span>
         <h2>${countryName} (${countryFoods.length} foods)</h2>
         
         <div class="food-search">
@@ -210,7 +210,7 @@ function showCountryFoods(countryName, layer) {
 
     // Initial render
     renderFoods();
-    document.querySelector('.close-popup').addEventListener('click', () => {
-        map.closePopup(currentPopup);
-    });
+    //document.querySelector('.close-popup').addEventListener('click', () => {
+      //  map.closePopup(currentPopup);
+   // });
 }
